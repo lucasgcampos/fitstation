c05776ac21e45225ff5c0c64ea5542355ff25aa3
lucascampos <lucasg_campos@hotmail.com>
2014-03-11 21:03:39 -0300

    responsive

--- WebContent/WEB-INF/cronometro.js
+++ WebContent/WEB-INF/cronometro.js
@@ -81,7 +81,7 @@ function mapear() {
 };
 
 /**
- * Pega a posi��o (longitude e a longitude) inicial do usu�rio
+ * Pega a posição (longitude e a longitude) inicial do usuário
  */
 function getInitLocal() {
 	navigator.geolocation.getCurrentPosition(function(position) {
@@ -93,7 +93,7 @@ function getInitLocal() {
 }
 
 /**
- * Exibir mapa com ponto centralizado na localiza��o do usu�rio
+ * Exibir mapa com ponto centralizado na localização do usuário
  */
 function configurarMapeamento() {
 	map.travelRoute({
@@ -107,7 +107,7 @@ function configurarMapeamento() {
 }
 
 /**
- * Desenha a roto percorrida pelo usu�rio em tempo real
+ * Desenha a roto percorrida pelo usuário em tempo real
  */
 function desenharRota(e) {
 	$('#instructions').append('<li>'+e.instructions+'</li>');
@@ -126,7 +126,7 @@ function desenharRota(e) {
 }
 
 /**
- * Exibir mapa com ponto centralizado na localiza��o do usu�rio
+ * Exibir mapa com ponto centralizado na localização do usuário
  */
 function exibirMapa() {
 	prettyPrint();
@@ -138,14 +138,14 @@ function exibirMapa() {
 }
 
 /**
- * Calcula o gasto de calorias do usu�rio ap�s uma corrida 
+ * Calcula o gasto de calorias do usuário após uma corrida 
  */
 function calcularCalorias() {
 	return (peso * minutos * 0.133).toFixed(2);
 }
 
 /**
- * Calcula a dist�ncia percorrida pelo usu�rio 
+ * Calcula a distância percorrida pelo usuário 
  */
 function calculateDistance(lat1, lon1, lat2, lon2) {
 	var R = 6371;
@@ -172,7 +172,7 @@ function printData() {
 }
 
 /**
- * Desliga o rastreamento, zera o cron�metro e finaliza a corrida.
+ * Desliga o rastreamento, zera o cronômetro e finaliza a corrida.
  */
 function end() {
 	caloria = calcularCalorias();
