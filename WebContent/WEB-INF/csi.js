b0ad35bb1b7c234d352677b875f92fbd1dba5ffc
lucascampos <lucasg_campos@hotmail.com>
2014-03-20 10:35:26 -0300

    refatoração I

--- WebContent/WEB-INF/csi.js
+++ WebContent/WEB-INF/csi.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 	var elements = document.getElementsByTagName('*'),
-		i;
+		i = 0;
 	for (i in elements) {
 		if (elements[i].hasAttribute && elements[i].hasAttribute('data-include')) {
 			fragment(elements[i], elements[i].getAttribute('data-include'));
